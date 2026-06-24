5fe3ced76b23fa4341b0da309faf239b3b95568a
Cody Venditto <cvenditto@proton.me>
2026-06-24 16:36:46 -0400

    Install Starwind and Fontsource

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+    },
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
