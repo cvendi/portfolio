3d95b91a99d183988b0b47902a5843428fab3cd8
Cody Venditto <cvenditto@proton.me>
2026-06-25 07:25:53 -0400

    fix: Add cloudflare adapter + image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare({
+    imageService: { build: "compile", runtime: "cloudflare-binding" },
+  }),
   fonts: [
     {
       provider: fontProviders.fontsource(),
